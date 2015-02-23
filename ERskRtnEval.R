edb97edb10e4e32bad89e8ee9d01d1389dd63006
unknown <piecepeace8p@gmail.com>
2015-02-23 23:59:02 +0900

    option position dynamism scenario using regression results continues

--- ERskRtnEval.R
+++ ERskRtnEval.R
@@ -149,7 +149,7 @@ getDTRRR(position=position)
 
 getVTRRR(position=position,ividx=getIV_td(position$IVIDX),dviv=annuual.daily.volatility(getIV_td(histIV$IVIDX))$daily)
 
-#Factory of Regression Results
+#Factory of Volatility Level Regression Result
 get.Volatility.Level.Regression<-function(Days=holdDays,ctoc=TRUE){
   if((!ctoc)*(Days==1)){
     return (PC1dCtO_IVCF1dCtO)
@@ -164,12 +164,69 @@ get.Volatility.Level.Regression<-function(Days=holdDays,ctoc=TRUE){
   }
 }
 
+#Factory of Volatility Change Regression Resulst
+get.Volatility.Change.Regression<-function(optype,up_dn){
+  if(optype==OpType_Put_G){
+    if(up_dn>=0){
+      return(PutIVChgUp)
+    }else {
+      return(PutIVChgDown)
+    }
+  }else if(optype==OpType_Call_G){
+    if(up_dn>=0){
+      return(CallIVChgUp)
+    }else {
+      return(CallIVChgDown)
+    }
+  }
+}
+
+#read from get.Volatility.Change.Regression.Result to get specific regression values.
+get.VolChg<-function(model,month){
+  chg<-predict(model,x=month)
+  return(chg)
+}
+
+#get the ATMIV_pos/ATMIV_pre (Result of Vchg regression) as a vector for each position element
+get.Volatility.Change.Regression.Result<-function(pos,up_dn){
+  atmiv_chg<-           (pos$TYPE==OpType_Put_G)*(up_dn>=0)*(get.VolChg(model=PutIVChgUp,month=pos$TimeToExpDate))$y
+  atmiv_chg<-atmiv_chg+(pos$TYPE==OpType_Call_G)*(up_dn>=0)*(get.VolChg(model=CallIVChgUp,month=pos$TimeToExpDate))$y
+  atmiv_chg<-atmiv_chg+(pos$TYPE==OpType_Put_G)*(up_dn<0)*(get.VolChg(model=PutIVChgDown,month=pos$TimeToExpDate))$y
+  atmiv_chg<-atmiv_chg+(pos$TYPE==OpType_Call_G)*(up_dn<0)*(get.VolChg(model=CallIVChgDown,month=pos$TimeToExpDate))$y
+  atmiv_chg
+}
+
+#read from et.Volatility.Cone.Regression.Result to get specific regression values.
+get.VCone<-function(model,month){
+  cone<-predict(model,x=month)
+  return(cone)
+}
+
+#get the ATMIV_pos/ATMIV_pre (Result of Vchg regression) as a vector for each position element
+get.Volatility.Cone.Regression.Result<-function(optype,month){
+  cone<-(optype==OpType_Put_G)*(get.VCone(model=PutVCone,month=month))$y
+  cone<-cone+(optype==OpType_Call_G)*(get.VCone(model=CallVCone,month=month))$y
+  cone
+}
+
 udly_chg_pct<-0.03
 #udly_chg_value<-mean(position$UDLY*udly_chg_pct)
 regression<-get.Volatility.Level.Regression()
+
+#IVIDX_pre/IVIDX_posに対するATMIV_pos/ATMIV_pre
 get.predicted.IVIDXChange(model=regression$model,xmin=udly_chg_pct,xmax=100,x_by=0) ;rm(regression,udly_chg_pct)
 #get.predicted.IVIDXChange(model=regression$model,xmin=udly_chg_pct,xmax=udly_chg_pct)
 #get.predicted.IVIDXChange(model=regression$model,
 #                          xmin=min(c(-udly_chg_pct,udly_chg_pct)),
 #                          xmax=max(c(-udly_chg_pct,udly_chg_pct)))
+get.Volatility.Change.Regression(optype=position$TYPE,up_dn=udly_chg_pct)
+
+#Volatility Cone の影響。時間変化した分の影響を受ける。その比の分だけ比率変化
+#上で求めたATMIV_pos <- ATMIV_pos*(ATMIV_pos/IVIDX_pos)t=TimeToExp_pre/(ATMIV_pos/IVIDX_pos)t=TimeToExp_pos
+bdays_per_month<-252/12
+timeToExp_pos<-(position$TimeToExp*bdays_per_month-holdDays)/bdays_per_month
+get.Volatility.Cone.Regression.Result(position$TYPE,position$TimeToExp)
+get.Volatility.Cone.Regression.Result(position$TYPE,timeToExp_pos)
+get.Volatility.Cone.Regression.Result(position$TYPE,timeToExp_pos)/
+  get.Volatility.Cone.Regression.Result(position$TYPE,position$TimeToExp)
 
