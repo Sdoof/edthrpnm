88fa999d5bb37fee6b5151b59155172b46cbebab
piecepeace <piecepeace8p@gmail.com>
2015-04-11 21:34:21 +0900

    calln putn posnum calculated

--- EResPostProcess.R
+++ EResPostProcess.R
@@ -10,27 +10,33 @@ library(ggplot2)
 #read from file to post process. reuse createCombineCandidatePool to get a data.frame
 
 ##
-# Exact
-res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\inipop-Exc-2015-4-01.csv",sep=""),
+# Exact (1Cb)
+res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\1Cb-2015-4-09.csv",sep=""),
                                 pnum=0,nrows=-1,skip=0,method=1)
-res1[,1:length(iniPos)] %>% rowwise() %>% do(s=sum( as.numeric(unlist(.)!=0) )) -> tmp
-unlist(tmp)
-res1$posnum<-unlist(tmp) ; rm(tmp)
-#平均以上のスコアだけ抽出
-res1 %>% filter(.[,length(iniPos)+1]<mean(res1[,length(iniPos)+1]))
-#top 5000
-res1 %>% arrange(.[,length(iniPos)+1]) %>% head(5000) -> res1
-  
+
+#above average score
+#res1 %>% filter(.[,length(iniPos)+1]<mean(res1[,length(iniPos)+1]))
+#over the specified socre
+res1 %>% filter(.[,length(iniPos)+1]<1.2) -> res1
+#posnum put call
+res1[,1:length(iniPos)] %>% rowwise() %>% do(putcalln=getPutCallnOfthePosition(unlist(.))) -> tmp
+tmp  %>% rowwise() %>% do(putn=(unlist(.)[1]),calln=(unlist(.)[2]))->tmp2
+res1$putn<-unlist(tmp2$putn);res1$calln<-unlist(tmp2$calln);rm(tmp);rm(tmp2)
+
 ##
-#  3Cb3Cb
-res2<-createCombineCandidatePool(fname=paste(".\\ResultData\\combine-Result-3Cb3Cb-2015-4-01.csv",sep=""),
+#  2Cb
+res2<-createCombineCandidatePool(fname=paste(".\\ResultData\\2Cb-2015-4-09.csv",sep=""),
                                  pnum=0,nrows=-1,skip=0,method=1)
 #必要のない列の削除
 res2 %>% select(1:(length(iniPos)+1)) -> res2
-#posnumの項目追加
-res2[,1:length(iniPos)] %>% rowwise() %>% do(s=sum( as.numeric(unlist(.)!=0) )) -> tmp
-unlist(tmp)
-res2$posnum<-unlist(tmp) ; rm(tmp)
+
+#over the specified socre
+res2 %>% filter(.[,length(iniPos)+1]<1.2) -> res2
+
+#posnum put call
+res2[,1:length(iniPos)] %>% rowwise() %>% do(putcalln=getPutCallnOfthePosition(unlist(.))) -> tmp
+tmp  %>% rowwise() %>% do(putn=(unlist(.)[1]),calln=(unlist(.)[2]))->tmp2
+res2$putn<-unlist(tmp2$putn);res2$calln<-unlist(tmp2$calln);rm(tmp);rm(tmp2)
 
 #
 #  total_res  一つにまとめたdata.frame. 
@@ -38,56 +44,68 @@ full_join(res1,res2) %>% arrange(.[,length(iniPos)+1])  %>% distinct() -> total_
 rm(res1,res2)
 
 ##
-#  2Cb
-res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\combine-Result-2CB-2015-4-01.csv",sep=""),
+#  3Cb
+res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\3Cb-2015-4-09.csv",sep=""),
                                  pnum=0,nrows=-1,skip=0,method=1)
 #必要のない列の削除
 res1 %>% select(1:(length(iniPos)+1)) -> res1
-#posnum
-res1[,1:length(iniPos)] %>% rowwise() %>% do(s=sum( as.numeric(unlist(.)!=0) )) -> tmp
-unlist(tmp)
-res1$posnum<-unlist(tmp) ; rm(tmp)
-#top 5000
-res1 %>% arrange(.[,length(iniPos)+1]) %>% head(5000) -> res1
+#over the specified socre
+res1 %>% filter(.[,length(iniPos)+1]<1.2) -> res1
+#posnum put call
+res1[,1:length(iniPos)] %>% rowwise() %>% do(putcalln=getPutCallnOfthePosition(unlist(.))) -> tmp
+tmp  %>% rowwise() %>% do(putn=(unlist(.)[1]),calln=(unlist(.)[2]))->tmp2
+res1$putn<-unlist(tmp2$putn);res1$calln<-unlist(tmp2$calln);rm(tmp);rm(tmp2)
 
 #full join
 full_join(total_res,res1) %>% arrange(.[,length(iniPos)+1])  %>% distinct() -> total_res
 
 ##
-#  3Cb
-res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\combine-Result-3Cb-2015-4-01.csv",sep=""),
+#  2Cb+2Cb
+res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\combine-Result-2Cb(+1Cb+1Cb)2Cb(+1Cb+1Cb)-2015-4-09.csv",sep=""),
                                  pnum=0,nrows=-1,skip=0,method=1)
 #必要のない列の削除
 res1 %>% select(1:(length(iniPos)+1)) -> res1
-#posnum
-res1[,1:length(iniPos)] %>% rowwise() %>% do(s=sum( as.numeric(unlist(.)!=0) )) -> tmp
-unlist(tmp)
-res1$posnum<-unlist(tmp) ; rm(tmp)
-#top 5000
-res1 %>% arrange(.[,length(iniPos)+1]) %>% head(5000) -> res1
+#over the specified socre
+res1 %>% filter(.[,length(iniPos)+1]<1.2) -> res1
+#posnum put call
+res1[,1:length(iniPos)] %>% rowwise() %>% do(putcalln=getPutCallnOfthePosition(unlist(.))) -> tmp
+tmp  %>% rowwise() %>% do(putn=(unlist(.)[1]),calln=(unlist(.)[2]))->tmp2
+res1$putn<-unlist(tmp2$putn);res1$calln<-unlist(tmp2$calln);rm(tmp);rm(tmp2)
+
+#full join
+full_join(total_res,res1) %>% arrange(.[,length(iniPos)+1])  %>% distinct() -> total_res
+
+##
+# 3Cb+3Cb
+res1<-createCombineCandidatePool(fname=paste(".\\ResultData\\combine-Result-3Cb(+1Cb+1Cb+1Cb)3Cb(+1Cb+1Cb+1Cb)-2015-4-09.csv",sep=""),
+                                 pnum=0,nrows=-1,skip=0,method=1)
+#必要のない列の削除
+res1 %>% select(1:(length(iniPos)+1)) -> res1
+#over the specified socre
+res1 %>% filter(.[,length(iniPos)+1]<1.2) -> res1
+#posnum put call
+res1[,1:length(iniPos)] %>% rowwise() %>% do(putcalln=getPutCallnOfthePosition(unlist(.))) -> tmp
+tmp  %>% rowwise() %>% do(putn=(unlist(.)[1]),calln=(unlist(.)[2]))->tmp2
+res1$putn<-unlist(tmp2$putn);res1$calln<-unlist(tmp2$calln);rm(tmp);rm(tmp2)
+
 #full join
 full_join(total_res,res1) %>% arrange(.[,length(iniPos)+1])  %>% distinct() -> total_res
 
 ##
 # 条件指定
-total_res %>%  filter(posnum<=8) %>% filter(.[,length(iniPos)+1]<1.2) ->tmp_fil; total_res <- tmp_fil
-#total_res %>% filter(.[,length(iniPos)+1]<495) -> total_res
-
+total_res %>% mutate(posn=(putn+calln)) -> total_res
+total_res %>%  filter(posn<=8) %>% filter(.[,length(iniPos)+1]<1.1) ->tmp_fil; total_res <- tmp_fil
+#profit impact
 total_res$profit<-NULL
 total_res[,1:length(iniPos)] %>% rowwise() %>% do(profit=getProfit(unlist(.),isDebug=FALSE,udlStepNum=4,udlStepPct=0.02)) -> tmp
 as.vector(unlist(tmp))
 total_res$profit<-as.vector(unlist(tmp)) ; rm(tmp)
 
-# res_df<-total_res
-# price_vec<-rep(0,times=0)
-# for(i in 1:nrow(res_df)){
-#   cat(unlist(res_df[i,1:length(iniPos)]),sep=",")
-#   x<-unlist(res_df[i,1:length(iniPos)])
-#   price<-getProfit(x,isDebug=FALSE,udlStepNum=4,udlStepPct=0.02)
-#   cat(",",price,",",i,"\n")
-#   price_vec<-as.numeric((i==1))*price+as.numeric((i!=1))*c(price_vec,price)
-# }
-# rm(i,x,res_df,x,price,price_vec)
+#callの数でフィルタかけてみる
+total_res %>% filter(calln>=2) %>% arrange(.[,length(iniPos)+1]) -> tmp_fil
+
+#profitでフィルタ
+total_res %>% filter(profit>90) %>% arrange(.[,length(iniPos)+1]) -> tmp_fil
 
 ##
 # , 形式での表示
@@ -109,12 +127,21 @@ unlist(total_res[38,1:length(iniPos)]) -> dfoptim_inipop_vec
 dfoptim_inipop_vec<-as.vector(dfoptim_inipop_vec)
 
 rm(genoud_inipop_vec,dfoptim_inipop_vec)
-
 rm(tmp,tmp_fil,res1,res2)
 rm(total_res)
 
 ##
 # Functions to be loaded -------------------------
+
+#create put and call pos num of the specified spread
+getPutCallnOfthePosition<-function(x){
+  type<-opchain$TYPE
+  putpos<-as.numeric(type==OpType_Put_G)
+  putn<-sum( as.numeric((putpos*x)!=0) )
+  callpos<-as.numeric(type==OpType_Call_G)
+  calln<-sum( as.numeric((callpos*x)!=0) )
+  return (c(putn,calln))
+}
 
 # create initial population vector from data frame
 createInitialPopulationVecForOptimizer<-function(res_df, cand_num){
