6b4a9826f4a374eff87a132c6188e45646cbe0d7
piecepeace <piecepeace@users.noreply.github.com>
2015-07-07 22:03:26 +0900

    Create ResultdataBkupEDTHORPtoEDrive.bat

---
+++ shell/ResultdataBkupEDTHORPtoEDrive.bat
@@ -0,0 +1,3 @@
+robocopy \\EDTHORP\Users\kuby\edthrpnm\ResultData\RUT E:\ResultData\1\RUT /E %1 %2 %3
+robocopy \\EDTHORP\Users\kuby\edthrpnm2\ResultData\RUT E:\ResultData\2\RUT /E %1 %2 %3
+robocopy \\EDTHORP\Users\kuby\edthrpnm3\ResultData\RUT E:\ResultData\3\RUT /E %1 %2 %3
