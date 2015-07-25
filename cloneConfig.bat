e86229f7c65e81574028fcadda0e113bcf81319c
piecepeace <piecepeace@users.noreply.github.com>
2015-07-25 18:12:44 +0900

    Delete cloneConfig.bat

--- cloneConfig.bat
+++
@@ -1,2 +0,0 @@
-$(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm2" > .\MarketData\Data\ConfigParameters2.csv
-$(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm3" > .\MarketData\Data\ConfigParameters3.csv
