1e7f14662b313cc048711361e83811a00428aa19
piecepeace <piecepeace8p@gmail.com>
2015-08-06 01:44:38 +0900

    Market Data Request by IB API

---
+++ IBRequest.R
@@ -0,0 +1,11 @@
+library(IBrokers)
+tws<-twsConnect()
+serverVersion(tws)
+reqCurrentTime(tws)
+reqContractDetails(tws,twsOption("RUT",conId = "200862535"))
+reqMktData(tws,twsOption("RUT",conId = "200862535"))
+
+twsDisconnect(tws)
+rm(tws)
+
+
