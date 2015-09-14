4919b245f1b58e7a0421b224cad7823c6870de4a
piecepeace <piecepeace8p@gmail.com>
2015-09-14 20:30:16 +0900

    edthrpnm4

--- cloneConfig.ps1
+++ cloneConfig.ps1
@@ -1,2 +1,3 @@
 $(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm2" | Out-File -Filepath .\MarketData\Data\ConfigParameters2.csv -Encoding default 
-$(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm3" | Out-File -Filepath .\MarketData\Data\ConfigParameters3.csv -Encoding default 
+$(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm3" | Out-File -Filepath .\MarketData\Data\ConfigParameters3.csv -Encoding default
+$(Get-Content ".\\MarketData\Data\ConfigParameters.csv") -replace "edthrpnm","edthrpnm4" | Out-File -Filepath .\MarketData\Data\ConfigParameters4.csv -Encoding default 
