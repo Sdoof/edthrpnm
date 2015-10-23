9a363a9f667a02de5e4e4bfe04b386a85679f5d3
piecepeace <piecepeace8p@gmail.com>
2015-10-23 21:56:14 +0900

    APITicket wriiten

--- ShellIvkSmpl.R
+++ ShellIvkSmpl.R
@@ -13,6 +13,10 @@ DataFiles_Path_G="C:\\Users\\kuby\\edthrpnm\\MarketData\\data\\"
 ConfigParameters<-read.table(paste(DataFiles_Path_G,ConfigFileName_G,sep=""),
                              row.names=1, comment.char="#",header=T,stringsAsFactors=F,sep=",")
 
+#Definition
+OpType_Put_G=as.numeric(ConfigParameters["OpType_Put_G",1])
+OpType_Call_G=as.numeric(ConfigParameters["OpType_Call_G",1])
+
 #File
 Underying_Symbol_G=ConfigParameters["Underying_Symbol_G",1]
 ResultFiles_Path_G=ConfigParameters["ResultFiles_Path_G",1]
@@ -72,7 +76,7 @@ if(length(grep("3", ConfigFileName_G))>=1){
 
 #Option Chain 
 rf<-paste(DataFiles_Path_G,Underying_Symbol_G,"_Positions_Pre.csv",sep="")
-opchain<-read.table(rf,header=T,sep=",",stringsAsFactors=FALSE)
+opchain<-read.table(rf,header=T,sep=",",stringsAsFactors=F)
 
 #join
 rf<-paste(ResultFiles_Path_G,theCand,sep="")
@@ -109,19 +113,55 @@ system(st) ;rm(st)
 st <- "powershell.exe -Command \" Remove-Item .\\ResultData\\EvalCnd-2.csv  \""
 system(st) ;rm(st)
 
-
+#Separate into two groups. Theta > 0 and Theta<=0(Gamma positive)
 evalPositions %>% filter(ThetaEffect>0) %>% distinct() -> evalPositions_Theta
 rownames(evalPositions_Theta) <- c(1:nrow(evalPositions_Theta))
 evalPositions %>% filter(ThetaEffect<=0) %>% distinct() -> evalPositions_Gamma
 rownames(evalPositions_Gamma) <- c(1:nrow(evalPositions_Gamma))
 
-#EvalPosition
-
+#Write EvalPosition files
 write.table(evalPositions,paste(ResultFiles_Path_G,Underying_Symbol_G,"_EvalPosition.csv",sep=""),row.names = F,col.names=F,sep=",",append=F)
 write.table(evalPositions_Theta,paste(ResultFiles_Path_G,Underying_Symbol_G,"_EvalPosition_Theta.csv",sep=""),row.names = F,col.names=F,sep=",",append=F)
 write.table(evalPositions_Gamma,paste(ResultFiles_Path_G,Underying_Symbol_G,"_EvalPosition_Gamma.csv",sep=""),row.names = F,col.names=F,sep=",",append=F)
 
+#Write APITicket files
+targetPositions=evalPositions
+fname=paste(ResultFiles_Path_G,Underying_Symbol_G,"_APITicket.txt",sep="")
+for(Counter in 1:nrow(targetPositions)){
+  opchain$Position = unlist(targetPositions[Counter,])[1:length(opchain$Position)]
+  opchain %>% dplyr::filter(Position!=0) -> thePosition
+  cat("#Ticket",Counter,file=fname,append=T,"\n")
+  writeIbAPITicket(out_text_file=fname,thePosition=thePosition,sep=",")
+  thePosition %>% mutate(TYPE=ifelse(TYPE==OpType_Put_G,"Put","Call")) -> thePosition
+  sink(fname, append = T)
+  print(thePosition)
+  sink()
+}
 
-rm(opchain,evalPositions)
-rm(ConfigFileName_G,DataFiles_Path_G,ConfigParameters,Underying_Symbol_G,ResultFiles_Path_G)
+targetPositions=evalPositions_Theta
+fname=paste(ResultFiles_Path_G,Underying_Symbol_G,"_APITicket_Theta.txt",sep="")
+for(Counter in 1:nrow(targetPositions)){
+  opchain$Position = unlist(targetPositions[Counter,])[1:length(opchain$Position)]
+  opchain %>% dplyr::filter(Position!=0) -> thePosition
+  cat("#Ticket",Counter,file=fname,append=T,"\n")
+  writeIbAPITicket(out_text_file=fname,thePosition=thePosition,sep=",")
+  thePosition %>% mutate(TYPE=ifelse(TYPE==OpType_Put_G,"Put","Call")) -> thePosition
+  sink(fname, append = T)
+  print(thePosition)
+  sink()
+}
 
+targetPositions=evalPositions_Gamma
+fname=paste(ResultFiles_Path_G,Underying_Symbol_G,"_APITicket_Gamma.txt",sep="")
+for(Counter in 1:nrow(targetPositions)){
+  opchain$Position = unlist(targetPositions[Counter,])[1:length(opchain$Position)]
+  opchain %>% dplyr::filter(Position!=0) -> thePosition
+  cat("#Ticket",Counter,file=fname,append=T,"\n")
+  writeIbAPITicket(out_text_file=fname,thePosition=thePosition,sep=",")
+  thePosition %>% mutate(TYPE=ifelse(TYPE==OpType_Put_G,"Put","Call")) -> thePosition
+  sink(fname, append = T)
+  print(thePosition)
+  sink()
+}
+
+rm(list=ls())
