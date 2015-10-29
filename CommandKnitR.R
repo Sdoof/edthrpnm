3dc18284f86caa95d00543b94b56ae6fd84ceb04
piecepeace <piecepeace8p@gmail.com>
2015-10-29 18:55:48 +0900

    use knit2html

--- CommandKnitR.R
+++ CommandKnitR.R
@@ -2,7 +2,9 @@ library(knitr)
 library(markdown)
 rm(list=ls())
 
-out<-knit("./ResultData/PosAnalysisResult.Rmd",encoding="UTF-8")
-markdownToHTML(out,output="./ResultData/PosAnalysisResult.html")
+out<-knit("./ResultData/PosAnalysisResult.Rmd",output ="./ResultData/PosAnalysisResult.md",encoding="UTF-8")
+
+knit2html(input   = out, output  = "./ResultData/PosAnalysisResult.html")
+#markdownToHTML(out,output="./ResultData/PosAnalysisResult.html",fragment.only=F)
 
 rm(list=ls())
