9bc47b2671bd04aa65390bf17df22cd714a4c366
piecepeace <piecepeace8p@gmail.com>
2015-11-02 22:31:11 +0900

    call evalfunc for last and today's legs

--- ETrackSpreadPosition.R
+++ ETrackSpreadPosition.R
@@ -2,21 +2,32 @@ library(RQuantLib)
 library(ggplot2)
 library(dplyr)
 library(pracma)
+## Preparation
+#  Load EOptimize.R's code chunk
 
 #Config File
-ConfigFileName_G="ConfigParameters.csv"
-DataFiles_Path_G="C:\\Users\\kuby\\edthrpnm\\MarketData\\data\\"
+#ConfigFileName_G="ConfigParameters.csv"
+#DataFiles_Path_G="C:\\Users\\kuby\\edthrpnm\\MarketData\\data\\"
 
-ConfigParameters<-read.table(paste(DataFiles_Path_G,ConfigFileName_G,sep=""),
-                             row.names=1, comment.char="#",header=T,stringsAsFactors=F,sep=",")
+#ConfigParameters<-read.table(paste(DataFiles_Path_G,ConfigFileName_G,sep=""),
+#                             row.names=1, comment.char="#",header=T,stringsAsFactors=F,sep=",")
 
 #Definition
-OpType_Put_G=as.numeric(ConfigParameters["OpType_Put_G",1])
-OpType_Call_G=as.numeric(ConfigParameters["OpType_Call_G",1])
+#OpType_Put_G=as.numeric(ConfigParameters["OpType_Put_G",1])
+#OpType_Call_G=as.numeric(ConfigParameters["OpType_Call_G",1])
 
 #File
-Underying_Symbol_G=ConfigParameters["Underying_Symbol_G",1]
-ResultFiles_Path_G=ConfigParameters["ResultFiles_Path_G",1]
+#Underying_Symbol_G=ConfigParameters["Underying_Symbol_G",1]
+#ResultFiles_Path_G=ConfigParameters["ResultFiles_Path_G",1]
+
+getPutCallLegNOfthePosition<-function(x){
+  type<-opchain$TYPE
+  putpos<-(type+OpType_Put_G)
+  putn<-sum( as.numeric((putpos*x)!=0) )
+  callpos<-(type+OpType_Call_G)
+  calln<-sum( as.numeric((callpos*x)!=0) )
+  return (c(putn,calln))
+}
 
 ###
 ## Create Last Day's positiojn
@@ -36,12 +47,9 @@ eval_pos_idx=3
 tmp<-read.table(EvalPos_fn,header=F,sep=",",colClasses="numeric")
 evaPos<-unlist(tmp[eval_pos_idx,])[1:length(opchain$Position)]
 
-#読み込まれたopchain$Positionが0であれば、evaPosをPositionとしてセット
-if(sum(opchain$Position!=0)==0){
-  opchain$Position<-evaPos
-}
+##get position where opchain$Position!=0
 
-#get position where opchain$Position!=0
+opchain$Position=evaPos
 opchain %>% dplyr::filter(Position!=0) -> position
 position %>% select(ExpDate,TYPE,Strike,Position) -> position
 
@@ -60,6 +68,23 @@ merge(opchain,position) %>% select(Date,ExpDate,TYPE,Strike,ContactName,Position
   arrange(Date,ExpDate,desc(TYPE),Strike) -> position
 
 position$Position<-ifelse(is.na(position$Position), 0, position$Position)
+
+#evaluation value of last day's Leg
+##
+# NOTICE! opchain behaves as a Global Variable for eval function(obj_Income_sgmd)
+# In the future, this specification should be modified. But for now, 3 steps are needed to call obj_Income_sgmd
+#  1. assign opchain (where Position!=0)
+#  2. assign posnum
+#  3. call obj_Income_sgmd()
+opchain<-position
+posnum<-sum(getPutCallLegNOfthePosition(opchain$Position))
+val<-obj_Income_sgmd(opchain$Position,EvalFuncSetting,isDebug=F,isDetail=F,
+                     udlStepNum=EvalFuncSetting$UdlStepNum,udlStepPct=EvalFuncSetting$UdlStepPct,
+                     maxposnum=EvalFuncSetting$Maxposnum,PosMultip=PosMultip,
+                     tail_rate=EvalFuncSetting$Tail_rate,lossLimitPrice=EvalFuncSetting$LossLimitPrice,
+                     Delta_Direct_Prf=EvalFuncSetting$Delta_Direct_Prf[posnum],Vega_Direct_Prf=EvalFuncSetting$Vega_Direct_Prf[posnum],
+                     Delta_Neutral_Offset=EvalFuncSetting$Delta_Neutral_Offset[posnum],Vega_Neutral_Offset=EvalFuncSetting$Vega_Neutral_Offset[posnum])
+print(val)
 
 ###
 ## Create Today's Day's position and show Differnces 
@@ -83,11 +108,7 @@ for(eval_pos_idx in Spreads){
   opchain=opchain_today
   
   evaPos<-unlist(EvalPos_table[eval_pos_idx,])[1:length(opchain$Position)]
-  
-  #読み込まれたopchain$Positionが0であれば、evaPosをPositionとしてセット
-  if(sum(opchain$Position!=0)==0){
-    opchain$Position<-evaPos
-  }
+  opchain$Position<-evaPos
   
   #get position where opchain$Position!=0
   opchain %>% dplyr::filter(Position!=0) -> position_today
@@ -112,7 +133,7 @@ for(eval_pos_idx in Spreads){
   #print(position)
   #print(position_today)
   
-  position$Position_Pre<-position$Positio
+  position$Position_Pre<-position$Position
   position$Position=NULL
   
   full_join(position,position_today) -> positon_join
@@ -124,6 +145,18 @@ for(eval_pos_idx in Spreads){
   
   print(positon_join)
   cat("pos idx",eval_pos_idx,"diff ",sum(abs(positon_join$DiffPos))," ")
+  
+  #evaluation value of today's eval_pos_idx'th Leg
+  opchain=position_today
+  posnum<-sum(getPutCallLegNOfthePosition(opchain$Position))
+  val<-obj_Income_sgmd(opchain$Position,EvalFuncSetting,isDebug=F,isDetail=F,
+                       udlStepNum=EvalFuncSetting$UdlStepNum,udlStepPct=EvalFuncSetting$UdlStepPct,
+                       maxposnum=EvalFuncSetting$Maxposnum,PosMultip=PosMultip,
+                       tail_rate=EvalFuncSetting$Tail_rate,lossLimitPrice=EvalFuncSetting$LossLimitPrice,
+                       Delta_Direct_Prf=EvalFuncSetting$Delta_Direct_Prf[posnum],Vega_Direct_Prf=EvalFuncSetting$Vega_Direct_Prf[posnum],
+                       Delta_Neutral_Offset=EvalFuncSetting$Delta_Neutral_Offset[posnum],Vega_Neutral_Offset=EvalFuncSetting$Vega_Neutral_Offset[posnum])
+  print(val)
+  
 }
 
 #savefile or create object
