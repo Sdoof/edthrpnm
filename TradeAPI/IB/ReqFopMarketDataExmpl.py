a81cb0e8831f9ae60942debfc32d6a4d4a365c2f
piecepeace <piecepeace8p@gmail.com>
2015-11-03 16:23:52 +0900

    data update

--- TradeAPI/IB/ReqFopMarketDataExmpl.py
+++ TradeAPI/IB/ReqFopMarketDataExmpl.py
@@ -243,12 +243,7 @@ def writeToFile(sectype,symbol):
 
 # -- main  ---------------------------------------------------------------------
 fxFutOpContractList = [
-                       makeFxFutOptContract(sym='JPY', exp='20151106', strike='', right='C', multip=''),
-                       makeFxFutOptContract(sym='JPY', exp='20151106', strike='', right='P', multip=''),
-                       makeFxFutOptContract(sym='EUR', exp='20151106', strike='', right='C', multip=''),
-                       makeFxFutOptContract(sym='EUR', exp='20151106', strike='', right='P', multip=''),
-                       makeFxFutOptContract(sym='GBP', exp='20151106', strike='', right='C', multip=''),
-                       makeFxFutOptContract(sym='GBP', exp='20151106', strike='', right='P', multip=''),
+                       makeFxFutOptContract(sym='JPY', exp='20151204', strike='', right='C', multip=''),
                        makeFxFutOptContract(sym='JPY', exp='20151204', strike='', right='C', multip=''),
                        makeFxFutOptContract(sym='JPY', exp='20151204', strike='', right='P', multip=''),
                        makeFxFutOptContract(sym='EUR', exp='20151204', strike='', right='C', multip=''),
