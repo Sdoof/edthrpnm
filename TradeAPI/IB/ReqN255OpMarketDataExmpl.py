7540c3a256be674a972231c0f83a4118599f0b32
piecepeace <piecepeace8p@gmail.com>
2016-09-03 00:47:44 +0900

    code and data updated

--- TradeAPI/IB/ReqN255OpMarketDataExmpl.py
+++ TradeAPI/IB/ReqN255OpMarketDataExmpl.py
@@ -7,7 +7,7 @@ import csv
 
 # -- globals  ------------------------------------------------------------------
 port_G = 7496
-clientId_G = 678
+clientId_G = 800
 
 nextOrderId = -1
 contractDetail = None
@@ -17,7 +17,6 @@ priceInfoDict = {}
 
 N225_Strike_Max=30000
 N225_Strike_Min=10000
-
 
 class ContractPrice:
     def __init__(self):
@@ -98,8 +97,9 @@ def TickPriceHandler(msg):
                                                                 priceInfoDict[contRtrvd.m_localSymbol].last,
                                                                 priceInfoDict[contRtrvd.m_localSymbol].close))
 
-# -- factory functions  -----------------------------------------------------------------
 
+
+# -- factory functions  -----------------------------------------------------------------
 def makeOptContract(sym, exp, strike, right):
     newOptContract = Contract()
     newOptContract.m_symbol = sym
@@ -112,27 +112,15 @@ def makeOptContract(sym, exp, strike, right):
     newOptContract.m_currency = "JPY"
     return newOptContract
 
-
-def makeFxFutContract(sym, exp, multip):
-    newFutContract = Contract()
-    newFutContract.m_symbol = sym
-    newFutContract.m_secType = "FUT"
-    newFutContract.m_expiry = exp
-    newFutContract.m_multiplier = multip
-    newFutContract.m_exchange = "GLOBEX"
-    newFutContract.m_currency = "USD"
-    return newFutContract
-
-
-def makeFxFutOptContract(sym, exp, strike, right, multip):
+def makeUSOptContract(sym, exp, strike, right):
     newOptContract = Contract()
     newOptContract.m_symbol = sym
-    newOptContract.m_secType = "FOP"
+    newOptContract.m_secType = "OPT"
     newOptContract.m_expiry = exp
     newOptContract.m_strike = strike
     newOptContract.m_right = right
-    newOptContract.m_multiplier = multip
-    newOptContract.m_exchange = "GLOBEX"
+    newOptContract.m_multiplier = 100
+    newOptContract.m_exchange = "SMART"
     newOptContract.m_currency = "USD"
     return newOptContract
 
@@ -147,6 +135,17 @@ def makeIndexContract(sym, exchange):
     newOptContract.m_currency = ""
     return newOptContract
 
+def makeUSIndexContract(sym, exchange):
+    newOptContract = Contract()
+    newOptContract.m_symbol = sym
+    newOptContract.m_secType = "IND"
+    newOptContract.m_expiry = ""
+    newOptContract.m_strike = ""
+    newOptContract.m_right = ""
+    newOptContract.m_exchange = exchange
+    newOptContract.m_currency = "USD"
+    return newOptContract
+
 def subscribeContractList(opCont,con):
     global nextOrderId
     global SPX_Strike_Max, SPX_Strike_Min, RUT_Strike_Max,RUT_Strike_Min
@@ -154,7 +153,7 @@ def subscribeContractList(opCont,con):
     theOrderId = nextOrderId
     con.reqContractDetails(theOrderId, opCont)
     #raw_input('wait for contractDetail press to continue')
-    sleep(2.5)
+    sleep(5)
     contractRemoveList = []
     for con_item in range(len(contractRestoreList)):
         con_each = contractRestoreList[con_item]
@@ -162,11 +161,15 @@ def subscribeContractList(opCont,con):
         #      (con_each.m_conId, con_each.m_localSymbol, con_each.m_secType, con_each.m_right, con_each.m_strike,
         #       con_each.m_expiry, con_each.m_symbol, con_each.m_multiplier, con_each.m_exchange))
         ### exclude condition
-        if (con_each.m_strike % 100) != 0:
+        #if (con_each.m_strike % 10) != 0:
+        #    contractRemoveList.append(con_each)
+        if con_each.m_secType == 'OPT' and con_each.m_symbol == 'SPX' and con_each.m_strike < SPX_Strike_Min:
             contractRemoveList.append(con_each)
-        elif con_each.m_secType == 'OPT' and con_each.m_symbol == 'N225' and con_each.m_strike < N225_Strike_Min:
+        elif con_each.m_secType == 'OPT' and con_each.m_symbol == 'SPX' and con_each.m_strike > SPX_Strike_Max:
             contractRemoveList.append(con_each)
-        elif con_each.m_secType == 'OPT' and con_each.m_symbol == 'N225' and con_each.m_strike > N225_Strike_Max:
+        elif con_each.m_secType == 'OPT' and con_each.m_symbol == 'RUT' and con_each.m_strike < RUT_Strike_Min:
+            contractRemoveList.append(con_each)
+        elif con_each.m_secType == 'OPT' and con_each.m_symbol == 'RUT' and con_each.m_strike > RUT_Strike_Max:
             contractRemoveList.append(con_each)
     # Remove
     for con_rmv_item in range(len(contractRemoveList)):
@@ -213,7 +216,7 @@ def writeToFile(sectype,symbol):
               (contract.m_strike, contract.m_localSymbol, contract_price.last,
                contract_price.bid, contract_price.ask, contract.m_expiry, contract.m_right))
         if sectype == 'OPT' and contract_price.last == '':
-            contract_price.last = (contract_price.bid + contract_price.ask)/2.0
+            contract_price.last = 0.0
         if sectype == 'OPT' and (contract_price.bid < 0 or contract_price.ask < 0) :
             continue
         if sectype == 'OPT' and contract.m_right == 'P':
@@ -222,26 +225,33 @@ def writeToFile(sectype,symbol):
              contract.m_right = '-1'
         #contract.m_expiry = datetime.datetime.strptime(contract.m_expiry,'%Y%m%d').strftime('%Y/%m/%d')
         if sectype == 'OPT' and symbol == contract.m_symbol:
-            writer_csv.writerow(
-                [str(contract.m_strike), contract.m_localSymbol, str(format(contract_price.last,'.10f')), str(format(contract_price.bid,'.10f')),
-                 str(format(contract_price.ask,'.10f')), datetime.strptime(contract.m_expiry,'%Y%m%d').strftime('%Y/%m/%d'), contract.m_right])
+            try:
+                writer_csv.writerow(
+                    [str(contract.m_strike), contract.m_localSymbol, str(format(contract_price.last,'.10f')), str(format(contract_price.bid,'.10f')),
+                     str(format(contract_price.ask,'.10f')), datetime.strptime(contract.m_expiry,'%Y%m%d').strftime('%Y/%m/%d'), contract.m_right])
+            except:
+                continue
         elif sectype == 'IND' and symbol == contract.m_symbol:
-            writer_csv.writerow(
-                [str(contract_price.last),str(contract_price.close),datetime.now().strftime('%Y/%m/%d %H:%M:%S')])
+            try:
+                writer_csv.writerow(
+                    [str(contract_price.last),str(contract_price.close),datetime.now().strftime('%Y/%m/%d %H:%M:%S')])
+            except:
+                continue
     file.close()
 
 # -- main  ---------------------------------------------------------------------
-opContractList = [makeOptContract(sym='N225', exp='20151210', strike='', right='C'),
-                  makeOptContract(sym='N225', exp='20151210', strike='', right='C'),
-                  makeOptContract(sym='N225', exp='20151210', strike='', right='P'),
-                  makeOptContract(sym='N225', exp='20160107', strike='', right='C'),
-                  makeOptContract(sym='N225', exp='20160107', strike='', right='P')]
+#18 elements finish in xx mins
+opContractList = [
+                  makeOptContract(sym='N225', exp='20161013', strike='', right='C'),
+                  makeOptContract(sym='N225', exp='20161013', strike='', right='P'),
+                  makeOptContract(sym='N225', exp='20161110', strike='', right='C'),
+                  makeOptContract(sym='N225', exp='20161110', strike='', right='P')
+]
 idxContractList = [makeIndexContract(sym='N225',exchange='OSE.JPN')]
 
 if __name__ == '__main__':
     # Server Access
     con = Connection.create(port=port_G, clientId=clientId_G)
-    # con.registerAll(MessageHandler)
     con.register(ErrorHandler, 'Error')
     con.register(NextValidIdHandler, 'NextValidId')
     con.register(MultiContractDetailsHandler, 'ContractDetails')
@@ -249,14 +259,9 @@ if __name__ == '__main__':
     con.register(TickPriceHandler, 'TickPrice')
     con.connect()
     con.setServerLogLevel(5)
-
-    # get mext Order Id
-    #raw_input('wait for nextOrderId')
+    # get next Order Id
     con.reqIds(1)
     sleep(1)
-
-    #raw_input('getting Fx Future Option Contract press any to continue')
-    print('getting N225 Option Contract')
 
     #Option Contract
     for opContract_item in range(len(opContractList)):
@@ -268,15 +273,15 @@ if __name__ == '__main__':
         #raw_input('requesting Fx Futre Option length press any to continue ')
         subscribeDataRequest(con)
         #raw_input('cancel mktData %s press any to continue' % (orderIdMktReqContractDict.keys()))
-        sleep(8)
+        sleep(13)
         for req_order_id in orderIdMktReqContractDict.iterkeys():
             con.cancelMktData(req_order_id)
         #raw_input('Price data writing to file press to continue')
         print('Price data writing to file press to continue')
-        sleep(2)
-        writeToFile(OpContract.m_secType,OpContract.m_symbol)
         con.disconnect()
-        sleep(3)
+        sleep(2)
+        clientId_G=clientId_G+1
+        print('New connection clientId %s' % (clientId_G))
         con = Connection.create(port=port_G, clientId=clientId_G)
         # con.registerAll(MessageHandler)
         con.register(ErrorHandler, 'Error')
@@ -287,6 +292,7 @@ if __name__ == '__main__':
         con.connect()
         con.setServerLogLevel(5)
         con.reqIds(1)
+        writeToFile(OpContract.m_secType,OpContract.m_symbol)
 
     # INDEX
     sleep(3)
@@ -298,15 +304,15 @@ if __name__ == '__main__':
         subscribeContractList(idxContract,con)
         #raw_input('requesting Fx Futre Option length press any to continue ')
         subscribeDataRequest(con)
-        sleep(5)
+        sleep(2)
         #raw_input('cancel mktData %s press any to continue' % (orderIdMktReqContractDict.keys()))
         for req_order_id in orderIdMktReqContractDict.iterkeys():
             con.cancelMktData(req_order_id)
         #raw_input('Price data writing to file press to continue')
-        sleep(2)
-        writeToFile(idxContract.m_secType,idxContract.m_symbol)
         con.disconnect()
-        sleep(3)
+        sleep(2)
+        clientId_G=clientId_G+1
+        print('New connection clientId %s' % (clientId_G))
         con = Connection.create(port=port_G, clientId=clientId_G)
         # con.registerAll(MessageHandler)
         con.register(ErrorHandler, 'Error')
@@ -317,13 +323,12 @@ if __name__ == '__main__':
         con.connect()
         con.setServerLogLevel(5)
         con.reqIds(1)
-
+        writeToFile(idxContract.m_secType,idxContract.m_symbol)
     print('About to exit press any to continue')
 
     # Receive the new OrderId sequence from the IB Server
     con.reqIds(1)
     sleep(2)
-
     # disconnect
     con.disconnect()
     sleep(3)
