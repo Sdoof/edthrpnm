6d9f2b92639e85121138b01adad499c9f4455962
piecepeace <piecepeace8p@gmail.com>
2016-10-09 22:55:12 +0900

    show spread spilit and reverse

--- TradeAPI/IB/ComboTicketReplace.py
+++ TradeAPI/IB/ComboTicketReplace.py
@@ -107,6 +107,67 @@ if __name__ == '__main__':
             LmitP_A=copy.deepcopy(LimitPrice_G)
             QTY_A=copy.deepcopy(QTY_G)
 
+            #print(ST_A, Exp_A, Str_A, Right_A, BS_A, CmbR_A, LmitP_A, QTY_A)
+            if max(CmbR_A)  >= 2:
+                #print(max(CmbR_A),CmbR_A.index(max(CmbR_A)),CmbR_A[0],CmbR_A[1],CmbR_A[2])
+
+                ST_A_BRKUP=[]
+                Exp_A_BRKUP=[]
+                Str_A_BRKUP=[]
+                Right_A_BRKUP=[]
+                BS_A_BRKUP=[]
+                CmbR_A_BRKUP=[]
+                for i in range(len(CmbR_A)):
+                    if i != CmbR_A.index(max(CmbR_A)):
+                        ST_A_BRKUP.append(ST_A[i])
+                        Exp_A_BRKUP.append(Exp_A[i])
+                        Str_A_BRKUP.append(Str_A[i])
+                        Right_A_BRKUP.append(Right_A[i])
+                        BS_A_BRKUP.append(BS_A[i])
+                        CmbR_A_BRKUP.append(CmbR_A[i])
+
+                #print(ST_A_BRKUP,Exp_A_BRKUP,Str_A_BRKUP,Right_A_BRKUP,BS_A_BRKUP,CmbR_A_BRKUP)
+
+                ST_A_BRK_RST=[]
+                Exp_A_BRK_RST = []
+                Str_A_BRK_RST = []
+                Right_A_BRK_RST = []
+                BS_A_BRK_RST = []
+                CmbR_A_BRK_RST = []
+
+                for i in range(len(ST_A_BRKUP)):
+                    ST_A_BRK_RST.append(  [ST_A[ CmbR_A.index(max(CmbR_A)) ],ST_A_BRKUP[i]]  )
+                    Exp_A_BRK_RST.append(  [Exp_A[ CmbR_A.index(max(CmbR_A)) ],Exp_A_BRKUP[i]] )
+                    Str_A_BRK_RST.append(  [Str_A[ CmbR_A.index(max(CmbR_A)) ],Str_A_BRKUP[i]] )
+                    Right_A_BRK_RST.append(  [Right_A[CmbR_A.index(max(CmbR_A))], Right_A_BRKUP[i]]  )
+                    BS_A_BRK_RST.append( [BS_A[CmbR_A.index(max(CmbR_A))], BS_A_BRKUP[i]] )
+                    #CmbR_A_BRK_RST.append( [CmbR_A[CmbR_A.index(max(CmbR_A))],CmbR_A_BRKUP[i]] )
+                    CmbR_A_BRK_RST.append( [  1  ,CmbR_A_BRKUP[i]] )
+
+                #print(ST_A_BRK_RST,Exp_A_BRK_RST,Str_A_BRK_RST,Right_A_BRK_RST,BS_A_BRK_RST,CmbR_A_BRK_RST)
+
+                print("\n### Ticket Split")
+                #print(ST_A_BRK_RST[0], Exp_A_BRK_RST[0], Str_A_BRK_RST[0], Right_A_BRK_RST[0], BS_A_BRK_RST[0], CmbR_A_BRK_RST[0],LmitP_A,QTY_A)
+                #print(ST_A_BRK_RST[1], Exp_A_BRK_RST[1], Str_A_BRK_RST[1], Right_A_BRK_RST[1], BS_A_BRK_RST[1],CmbR_A_BRK_RST[1], LmitP_A, QTY_A)
+                for i in range(len(ST_A_BRKUP)):
+                    print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
+                          "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" % (
+                          ST_A_BRK_RST[i], Exp_A_BRK_RST[i], Str_A_BRK_RST[i], Right_A_BRK_RST[i], BS_A_BRK_RST[i],
+                          CmbR_A_BRK_RST[i], LmitP_A, QTY_A))
+
+                print("\n### Ticket Split Reversed")
+                for i in range(len(ST_A_BRKUP)):
+                    print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
+                          "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" % (
+                              list(reversed(ST_A_BRK_RST[i])), list(reversed(Exp_A_BRK_RST[i])), list(reversed(Str_A_BRK_RST[i])), list(reversed(Right_A_BRK_RST[i])), list(reversed(BS_A_BRK_RST[i])),
+                              list(reversed(CmbR_A_BRK_RST[i])), LmitP_A, QTY_A))
+            else:
+                print("\n### Ticket Reversed")
+                print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
+                  "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" %
+                      (list(reversed(ST_A)), list(reversed(Exp_A)), list(reversed(Str_A)), list(reversed(Right_A)),
+                       list(reversed(BS_A)), list(reversed(CmbR_A)), LmitP_A, QTY_A))
+
             ##
             # Second Ticket
             tkpkg = queTickets.popleft()
@@ -133,6 +194,8 @@ if __name__ == '__main__':
                        Leg[legitem].m_right, Leg[legitem].m_strike, Leg[legitem].m_expiry))
             raw_input('Spread leg info correct?')
 
+
+
             # deep copy for replacement
             ST_B = copy.deepcopy(SymbolTicket)
             Exp_B = copy.deepcopy(ExpiryTicket)
@@ -143,10 +206,17 @@ if __name__ == '__main__':
             LmitP_B = copy.deepcopy(LimitPrice_G)
             QTY_B = copy.deepcopy(QTY_G)
 
+            #Ticket Reversed
+            print("\n###  Ticket Reversed")
+            print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
+                  "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" %
+                  (list(reversed(ST_B)), list(reversed(Exp_B)), list(reversed(Str_B)), list(reversed(Right_B)),
+                   list(reversed(BS_B)), list(reversed(CmbR_B)), LmitP_B, QTY_B))
+
             #Original combo
-            print("\nOriginal Ticket Elements")
-            print(ST_A, Exp_A, Str_A, Right_A, BS_A, CmbR_A, LmitP_A, QTY_A)
-            print(ST_B, Exp_B, Str_B, Right_B, BS_B, CmbR_B, LmitP_B, QTY_B)
+            #print("\nOriginal Ticket Elements")
+            #print(ST_A, Exp_A, Str_A, Right_A, BS_A, CmbR_A, LmitP_A, QTY_A)
+            #print(ST_B, Exp_B, Str_B, Right_B, BS_B, CmbR_B, LmitP_B, QTY_B)
 
             ##Replacing
             TMP_ST=ST_A[1]
@@ -174,7 +244,7 @@ if __name__ == '__main__':
             #print(ST_B, Exp_B, Str_B, Right_B, BS_B, CmbR_B, LmitP_B, QTY_B)
 
             #Replaced Tickets
-            print("\nTicket Replaced")
+            print("\n###  Ticket Replaced")
             print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
                   "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" %
                   (ST_A,Exp_A,Str_A,Right_A,BS_A,CmbR_A,LmitP_A,QTY_A))
@@ -183,7 +253,7 @@ if __name__ == '__main__':
                   (ST_B, Exp_B, Str_B, Right_B, BS_B, CmbR_B, LmitP_B, QTY_B))
 
             #Replaced Tickets Reversed
-            print("\nReplaced Ticket Reversed")
+            print("\n###  Replaced Ticket Reversed")
             print("SymbolTicket = %s; ExpiryTicket = %s; StrikeTicket = %s; RightTicket = %s; "
                   "BuySell = %s; ComboRatio = %s; LimitPrice_G = %s; QTY_G =%s" %
                   (list(reversed(ST_A)),list(reversed(Exp_A)),list(reversed(Str_A)),list(reversed(Right_A)),
