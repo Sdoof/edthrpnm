e21a62ca6d624c40f79e8204a1662806ce2d69d5
piecepeace <piecepeace@users.noreply.github.com>
2016-10-14 22:19:41 +0900

    Create gitshellColorChange.ps1

---
+++ samples/gitshellColorChange.ps1
@@ -0,0 +1,4 @@
+git config --global color.status.changed "cyan normal bold" 
+git config --global color.status.untracked "cyan normal bold"
+#$global:GitPromptSettings.WorkingForegroundColor    = [ConsoleColor]::Yellow 
+#global:GitPromptSettings.UntrackedForegroundColor  = [ConsoleColor]::Yellow
