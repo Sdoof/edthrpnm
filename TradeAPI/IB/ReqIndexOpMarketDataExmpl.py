56b2bbbdf49b5c215b0659377aa0f22da21aca6b
piecepeace <piecepeace8p@gmail.com>
2017-01-14 00:11:12 +0900

    data update

--- TradeAPI/IB/ReqIndexOpMarketDataExmpl.py
+++ TradeAPI/IB/ReqIndexOpMarketDataExmpl.py
@@ -247,8 +247,6 @@ def writeToFile(sectype,symbol):
 # -- main  ---------------------------------------------------------------------
 #18 elements finish in 4min15s
 opContractList = [
-    makeOptContract(sym='SPX', exp='20170216', strike='', right='P'),
-    makeOptContract(sym='SPX', exp='20170216', strike='', right='C'),
     makeOptContract(sym='SPX', exp='20170228', strike='', right='P'),
     makeOptContract(sym='SPX', exp='20170228', strike='', right='C'),
     makeOptContract(sym='SPX', exp='20170316', strike='', right='P'),
@@ -261,8 +259,6 @@ opContractList = [
     makeOptContract(sym='SPX', exp='20170428', strike='', right='C'),
     makeOptContract(sym='SPX', exp='20170531', strike='', right='P'),
     makeOptContract(sym='SPX', exp='20170531', strike='', right='C'),
-    makeOptContract(sym='RUT', exp='20170216', strike='', right='P'),
-    makeOptContract(sym='RUT', exp='20170216', strike='', right='C'),
     makeOptContract(sym='RUT', exp='20170228', strike='', right='P'),
     makeOptContract(sym='RUT', exp='20170228', strike='', right='C'),
     makeOptContract(sym='RUT', exp='20170316', strike='', right='P'),
@@ -270,7 +266,9 @@ opContractList = [
     makeOptContract(sym='RUT', exp='20170331', strike='', right='P'),
     makeOptContract(sym='RUT', exp='20170331', strike='', right='C'),
     makeOptContract(sym='RUT', exp='20170428', strike='', right='P'),
-    makeOptContract(sym='RUT', exp='20170428', strike='', right='C')
+    makeOptContract(sym='RUT', exp='20170428', strike='', right='C'),
+    makeOptContract(sym='RUT', exp='20170531', strike='', right='P'),
+    makeOptContract(sym='RUT', exp='20170531', strike='', right='C')
 ]
 
 idxContractList = [
