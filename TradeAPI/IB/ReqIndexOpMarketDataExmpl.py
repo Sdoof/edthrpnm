ae66fde032534b248983a9595cbf44d672ad3422
piecepeace <piecepeace8p@gmail.com>
2017-06-10 12:29:20 +0900

    data update

--- TradeAPI/IB/ReqIndexOpMarketDataExmpl.py
+++ TradeAPI/IB/ReqIndexOpMarketDataExmpl.py
@@ -15,12 +15,12 @@ contractRestoreList = None
 orderIdMktReqContractDict = None
 priceInfoDict = {}
 
-SPX_Strike_Max=2700
-SPX_Strike_Min=1800
+SPX_Strike_Max=2650
+SPX_Strike_Min=1900
 SPX_Strike_Max_P=SPX_Strike_Max-150
 SPX_Strike_Min_C=SPX_Strike_Min+250
-RUT_Strike_Max=1700
-RUT_Strike_Min=900
+RUT_Strike_Max=1750
+RUT_Strike_Min=850
 
 class ContractPrice:
     def __init__(self):
